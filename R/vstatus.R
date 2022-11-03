972ddc1e014f00bcf842a619be222fdff3850822
Alejandro Caravaca Puchades <acaravacapuchades@icloud.com>
2022-11-03 22:00:25 +0100

    Integrate dataentry_visits into vstatus source

--- R/vstatus.R
+++ R/vstatus.R
@@ -1,24 +1,38 @@
 source("R/anonimize.R")
 source("R/ufmn.R")
+source("R/dataentry.R")
 
-vstatus_alive <- ufmn_patients %>%
-    dplyr::filter(
-        exitus != TRUE & is.na(fecha_exitus),
-    ) %>%
-    dplyr::left_join(
+vstatus_all <- ufmn_patients %>%
+    mutate(apellidos = paste(primer_apellido, segundo_apellido)) %>%
+    left_join(
         ufmn_followups %>%
             group_by(pid) %>%
-            arrange(desc(fecha_visita), .by_group = TRUE) %>%
-            summarize(ultima_visita = first(fecha_visita)),
+            summarize(ultima_visita = max(fecha_visita)),
         by = "pid"
     ) %>%
-    select(
-        pid, nhc, cip, nombre, primer_apellido, segundo_apellido, ultima_visita
-    )
+    full_join(
+        dataentry_visits %>%
+            group_by(nhc) %>%
+            mutate(ultima_visita = max(fecha_visita)) %>%
+            filter(fecha_visita == ultima_visita),
+        by = "nhc", suffix = c("_ufmn", "_dataentry")
+    ) %>%
+    mutate(
+        nombre = coalesce(nombre_ufmn, nombre_dataentry),
+        apellidos = coalesce(apellidos_ufmn, apellidos_dataentry),
+        ultima_visita = pmax(ultima_visita_ufmn, ultima_visita_dataentry, na.rm = TRUE)
+    ) %>%
+    select(pid, nhc, nombre, apellidos, exitus, fecha_exitus, ultima_visita)
+
+vstatus_alive <- vstatus_all %>%
+    filter(exitus %in% c(FALSE, NA), is.na(fecha_exitus)) %>%
+    select(-c(exitus, fecha_exitus))
 
 vstatus_export <- function(path) {
     if (!file.exists(path)) {
         dir.create(path, recursive = TRUE)
     }
+
+    readr::write_csv(vstatus_all, file.path(path, "vstatus-all.csv"))
     readr::write_csv(vstatus_alive, file.path(path, "vstatus-alive.csv"))
 }
