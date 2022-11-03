1dfea1caac3d81c92ff72a13851eb4fbfdbd2196
Alejandro Caravaca Puchades <acaravacapuchades@icloud.com>
2022-11-03 22:00:06 +0100

    Add dataentry source

---
+++ R/dataentry.R
@@ -0,0 +1,17 @@
+library(dplyr)
+
+dataentry_data_path <- "data/dataentry-2022_11_03.xlsx"
+
+dataentry_visits <-
+    readxl::read_excel(dataentry_data_path, range = cellranger::cell_cols("B:F")) %>%
+    setNames(c("anterior", "primera_visita", "nhc", "paciente", "fecha_visita")) %>%
+    extract(
+        paciente,
+        into = c("apellidos", "nombre", "sexo", "edad"),
+        r"(^([A-ZÑ\s]+) ?, ([A-ZÑ\s]+) \(([HD]),(\d+)A\)$)", convert = TRUE
+    ) %>%
+    mutate(
+        across(fecha_visita, as.Date),
+        sexo = recode(sexo, H = "Hombre", D = "Mujer"),
+    ) %>%
+    select(nhc, nombre, apellidos, sexo, edad, fecha_visita)
