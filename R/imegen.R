1c49fcadbe978851f42e15dc246ac6ce511dedf0
Alejandro Caravaca Puchades <acaravacapuchades@icloud.com>
2022-11-04 18:13:32 +0100

    Implement imegen source

---
+++ R/imegen.R
@@ -0,0 +1,12 @@
+
+imegen_data_path <- "data/imegen-2022_11_04.xlsx"
+
+imegen_results <-
+    readxl::read_excel(imegen_data_path) %>%
+    setNames(c(
+        "id", "nhc", "panel", "gen", "alelo1", "alelo2", "HGVSc", "HGVSp",
+        "dbSNP", "maf", "genotipo", "herencia", "interpretacion"
+    )) %>%
+    dplyr::mutate(
+        across(c(genotipo, interpretacion, herencia), factor),
+    )
